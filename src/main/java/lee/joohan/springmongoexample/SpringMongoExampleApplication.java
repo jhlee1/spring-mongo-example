d276a968d7b312fe9ee1d061245a3ecc05032482
Joohan Lee <joohan33@gmail.com>
2019-12-31 11:03:09 +0900

    - apm agent 추가

--- src/main/java/lee/joohan/springmongoexample/SpringMongoExampleApplication.java
+++ src/main/java/lee/joohan/springmongoexample/SpringMongoExampleApplication.java
@@ -1,11 +1,13 @@
 package lee.joohan.springmongoexample;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMongoExampleApplication {
   public static void main(String[] args) {
+    ElasticApmAttacher.attach();
     SpringApplication.run(SpringMongoExampleApplication.class, args);
   }
 }
